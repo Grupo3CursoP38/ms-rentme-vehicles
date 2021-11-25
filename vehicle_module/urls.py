3dc46d8f042b0c1ac21baf09319a8985412c0250
Santiago Meneses <santiagome2014@hotmail.com>
2021-11-25 01:51:58 -0500

    swagger conf changed

--- vehicle_module/urls.py
+++ vehicle_module/urls.py
@@ -29,9 +29,9 @@ from drf_yasg import openapi
 #Swagger Configuration.
 schema_view = get_schema_view(
     openapi.Info(
-        title="Rentme-auth API",
+        title="ms-rentme-vehicles",
         default_version='v1',
-        description="Documentación de Endpoints para el backend de Autenticación. (Django)",
+        description="Documentación de Endpoints para el backend de Vehiculos. (Django)",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="sntiagomeneses@gmail.com"),
         license=openapi.License(name="BSD License"),
